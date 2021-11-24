6eefc20748be09c68421e413a71fd49245e61ab2
faheemjailor <faheemjailor0055>
2021-11-24 22:11:21 +0530

    faheem UC1 for Max number

---
+++ Generics/GenericClass.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsProgram
+{
+    class Generic
+    {
+
+        //This method created for maximum int number
+        public static int MaxNumber(int first, int second, int third)
+        {
+            if (first.CompareTo(second) > 0 && first.CompareTo(third) > 0)      //comparing values to each other by using compareTo keyword
+            {
+                return first;                                                  //returning value if condition is true
+            }
+            if (second.CompareTo(first) > 0 && second.CompareTo(third) > 0)
+            {
+                return second;
+            }
+            if (third.CompareTo(first) > 0 && third.CompareTo(second) > 0)
+            {
+                return third;
+            }
+            return default;
+        }
+    }
+}
